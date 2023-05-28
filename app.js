066c62197a6ce14b4013b35a8ec74fbf489a5c96
StevenP-2019-8372 <20198372@itla.edu.do>
2023-05-28 14:22:19 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const botonSubir = document.querySelector(".boton-subir");
+
+// Ocultar botón al inicio
+botonSubir.style.display = "none";
+
+window.addEventListener("scroll", () => {
+  // Mostrar botón cuando el usuario se desplaza hacia abajo
+  if (window.scrollY > 100) {
+    botonSubir.style.display = "block";
+  } else {
+    botonSubir.style.display = "none";
+  }
+});
+
+botonSubir.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+/*enviar formulario*/
+
+const btn = document.getElementById('button');
+
+document.getElementById('formulario-contacto')
+.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  btn.value = 'Sending...';
+
+  const serviceID = 'default_service';
+  const templateID = 'template_s30uh9f';
+
+  emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Enviado correctamente!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
+
